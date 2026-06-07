a2f7f828b762e04dca0cba3a0df2eeb031a7a3d5
manidevops-cloud <manikantapandi211@gmai.com>
2026-06-07 22:42:07 +0530

    hello world script created

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -4,4 +4,4 @@ PERSON1=Tramp
 PERSON2=Iran
 
 echo " $PERSON1 saying hey $PERSON2 I'll blast you soon be ready "
-echo " $PERSON2 replying haha hey $PERSON I'm waiting "
+echo " $PERSON2 replying haha hey $PERSON1 I'm waiting "
