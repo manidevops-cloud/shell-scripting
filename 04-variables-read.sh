15a63ad9da7f0c629417498a22febc336c6a99f4
manidevops-cloud <manikantapandi211@gmai.com>
2026-06-08 22:49:17 +0530

    hello world script created

--- 04-variables-read.sh
+++ 04-variables-read.sh
@@ -2,7 +2,7 @@
 
 echo " Please enter your user name "
 
-read USER_NAME # read 
+read USER_NAME # here USER_NAME is variable, whatever you enter in terminal stores in this variable
 
 echo " Your user name is $USER_NAME "
 
