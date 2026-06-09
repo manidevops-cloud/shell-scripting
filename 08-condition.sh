5a6670823340ffb90694aac630a6ade7f1c5ffea
manidevops-cloud <manikantapandi211@gmai.com>
2026-06-09 23:06:59 +0530

    hello world script created

--- 08-condition.sh
+++ 08-condition.sh
@@ -9,16 +9,11 @@ NUMBER=$1
 # ge - greater than or equal
 # le - less than or equal
 
-if [ $NUMBER -gt 20 ]; than
-
+if [ $NUMBER -gt 20 ]; then
 echo " the number is $NUMBER bigger than 20 "
-
-elif [ $NUMBER -eq 20 ]; than
-
+elif [ $NUMBER -eq 20 ]; then
 echo " the number is $NUMBER equel to 20 "
-
 else [ $NUMBER -le 20 ]
-
 echo " the number is $NUMBER lessthan 20 "
-
 fi
+
