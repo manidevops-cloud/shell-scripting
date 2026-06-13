ecdaec06b8c79ce5988dbab4f847dad520c98ce1
manidevops-cloud <manikantapandi211@gmai.com>
2026-06-13 11:15:45 +0530

    hello world script created

--- 10-logs.sh
+++ 10-logs.sh
@@ -24,10 +24,10 @@ fi
 # second arg -> exit code
 VALIDATE(){
     if [ $2 -ne 0 ]; then
-        echo "Installing $1 is ... FAILED" tee -a |$LOGS_FILE
+        echo "Installing $1 is ... FAILED" | tee -a $LOGS_FILE
         exit 1
     else
-        echo "Installing $1 is ... SUCCESS" tee -a |$LOGS_FILE
+        echo "Installing $1 is ... SUCCESS" | tee -a $LOGS_FILE
     fi
 }
 
@@ -35,7 +35,7 @@ VALIDATE(){
 dnf list installed mysql &>> $LOGS_FILE
 
 if [ $? -eq 0 ]; then
-    echo "MySQL is already installed ... SKIPPING" tee -a |$LOGS_FILE
+    echo "MySQL is already installed ... SKIPPING" | tee -a $LOGS_FILE
 else
     echo "Installing MySQL"
     dnf install mysql -y &>> $LOGS_FILE
@@ -44,7 +44,7 @@ fi
 
 dnf list installed nginx  &>> $LOGS_FILE
 if [ $? -eq 0 ]; then
-    echo "nginx is already installed ... SKIPPING" tee -a |$LOGS_FILE
+    echo "nginx is already installed ... SKIPPING" | tee -a $LOGS_FILE
 else
     echo "Installing nginx"
     dnf install nginx -y  &>> $LOGS_FILE
