1b6e2bc64d5e635069891f069b41dfe4ba2a0882
manidevops-cloud <manikantapandi211@gmai.com>
2026-06-13 10:51:57 +0530

    hello world script created

---
+++ 10-logs.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Checks whether the script is executed by the root user.
+# Uses a reusable VALIDATE() function for error handling.
+# Checks if MySQL is already installed.
+# If installed, skip installation.
+# Otherwise, install it.
+# Checks if Nginx is already installed.
+# If installed, skip installation.
+# Otherwise, install it.
+# Displays success or failure messages for each package installation.
+
+USERID=$(id -u)
+LOGS_DIR=/var/log/shell-scriptslogs
+LOGS_FILE="$LOGS_DIR/$0.log" # /home/ec2-user/shell-logs/10-logs.sh.log
+
+# Check root access or not
+if [ $USERID -ne 0 ]; then
+    echo "Please run this script with root access"
+    exit 1
+fi
+
+# first arg -> what are you trying to install
+# second arg -> exit code
+VALIDATE(){
+    if [ $2 -ne 0 ]; then
+        echo "Installing $1 is ... FAILED" tee -a |$LOGS_FILE
+        exit 1
+    else
+        echo "Installing $1 is ... SUCCESS" tee -a |$LOGS_FILE
+    fi
+}
+
+# echo "I am continuing..."
+dnf list installed mysql &>> $LOGS_FILE
+
+if [ $? -eq 0 ]; then
+    echo "MySQL is already installed ... SKIPPING" tee -a |$LOGS_FILE
+else
+    echo "Installing MySQL"
+    dnf install mysql -y &>> $LOGS_FILE
+    VALIDATE MySQL $?
+fi
+
+dnf list installed nginx  &>> $LOGS_FILE
+if [ $? -eq 0 ]; then
+    echo "nginx is already installed ... SKIPPING" tee -a |$LOGS_FILE
+else
+    echo "Installing nginx"
+    dnf install nginx -y  &>> $LOGS_FILE
+    VALIDATE nginx $?
+fi
